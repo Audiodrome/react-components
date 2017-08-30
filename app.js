beba430224694f65e03b7288ddae4b46767c2e7b
Hack Reactor Student <student@hackreactor.com>
2017-08-30 14:33:15 -0700

    save

--- app.js
+++ app.js
@@ -1,1 +1,59 @@
 // TODO
+
+let App = () => (
+    <div>
+        <h2>My Todo List</h2>
+        <TodoList todos={['Learn React', 'Crush Recast.ly', 'Maybe sleep']}/>
+    </div>
+);
+
+let TodoList = (props) => (
+    <ul>
+        {props.todos.map((todo, i) =>
+            <TodoListItem todo={todo} key={i} />
+        )}
+    </ul>
+);
+
+class TodoListItem extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <li>{this.props.todo}</li>
+        );
+    }
+
+}
+
+let Grocery = () => (
+    <div>
+        <h2>Grocery List</h2>
+        <GroceryList items={['Peanut Butter', 'Bananas', 'Tea']}/>
+    </div>
+);
+
+let GroceryList = (props) => (
+    <ul>
+        {props.items.map(item =>
+            <GroceryListItem item={item} />
+        )}
+    </ul>
+);
+
+class GroceryListItem extends React.Component {
+    
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <li>{this.props.item}</li>
+        );
+    }
+}
+
+ReactDOM.render(<Grocery />, document.getElementById('app'));

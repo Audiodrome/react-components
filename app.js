2514f1d3e34a588e4e20b54e72730ac177efffc8
Hack Reactor Student <student@hackreactor.com>
2017-08-31 16:27:44 -0700

    save

--- app.js
+++ app.js
@@ -1,59 +1,104 @@
 // TODO
-
-let App = () => (
-    <div>
-        <h2>My Todo List</h2>
-        <TodoList todos={['Learn React', 'Crush Recast.ly', 'Maybe sleep']}/>
-    </div>
-);
-
-let TodoList = (props) => (
-    <ul>
-        {props.todos.map((todo, i) =>
-            <TodoListItem todo={todo} key={i} />
-        )}
-    </ul>
-);
-
+// A class component can be defined as an ES6 class
+// that extends the base Component class included in the React library
 class TodoListItem extends React.Component {
 
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
 
-    render() {
-        return (
-            <li>{this.props.todo}</li>
-        );
-    }
+    this.state = {
+      done: false
+    };
+  }
 
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none'
+    };
+
+    return (
+      <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.todo}</li>
+    );
+  }
 }
 
-let Grocery = () => (
-    <div>
-        <h2>Grocery List</h2>
-        <GroceryList items={['Peanut Butter', 'Bananas', 'Tea']}/>
-    </div>
+var TodoList = (props) => (
+  <ul>
+    {props.todos.map(todo =>
+      <TodoListItem todo={todo} />
+    )}
+  </ul>
 );
 
-let GroceryList = (props) => (
-    <ul>
-        {props.items.map(item =>
-            <GroceryListItem item={item} />
-        )}
-    </ul>
+var App = () => (
+  <div>
+    <h2>My Todo List</h2>
+    <TodoList todos={['Learn React', 'Crush Recast.ly', 'Maybe sleep']}/>
+  </div>
 );
 
-class GroceryListItem extends React.Component {
+ReactDOM.render(<App />, document.getElementById('app'));
+// let App = () => (
+//     <div>
+//         <h2>My Todo List</h2>
+//         <TodoList todos={['Learn React', 'Crush Recast.ly', 'Maybe sleep']}/>
+//     </div>
+// );
+
+// let TodoList = (props) => (
+//     <ul>
+//         {props.todos.map((todo, i) =>
+//             <TodoListItem todo={todo} key={i} />
+//         )}
+//     </ul>
+// );
+
+// class TodoListItem extends React.Component {
+
+//     constructor(props) {
+//         super(props);
+//     }
+
+//     render() {
+//         return (
+//             <li>{this.props.todo}</li>
+//         );
+//     }
+
+// }
+
+// let Grocery = () => (
+//     <div>
+//         <h2>Grocery List</h2>
+//         <GroceryList items={['Peanut Butter', 'Bananas', 'Tea']}/>
+//     </div>
+// );
+
+// let GroceryList = (props) => (
+//     <ul>
+//         {props.items.map(item =>
+//             <GroceryListItem item={item} />
+//         )}
+//     </ul>
+// );
+
+// class GroceryListItem extends React.Component {
     
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
-            <li>{this.props.item}</li>
-        );
-    }
-}
+//     constructor(props) {
+//         super(props);
+//     }
+//     render() {
+//         return (
+//             <li>{this.props.item}</li>
+//         );
+//     }
+// }
 
-ReactDOM.render(<Grocery />, document.getElementById('app'));
+
